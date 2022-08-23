19ea3a1e06c29170834dc10ab97871ca9706389e
ssghost <ssprof0@gmail.com>
2022-08-23 23:43:56 +0800

    .

--- src/layout.jl
+++ src/layout.jl
@@ -6,7 +6,8 @@ import ./components as Cpn
 export create_layout = Dash.html_div() do
     Dash.title("My Gochess Page")
     Cpn.render_canvas()
-    Cpn.render_span()
+    Cpn.render_showturn()
+    Cpn.render_result()
     Cpn.render_restart()
     Cpn.render_endcheck()
 end
