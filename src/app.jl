34eedc0ff814d89ffc55e8fe4dc1272217467460
ssghost <ssprof0@gmail.com>
2022-08-23 23:40:34 +0800

    ...

--- src/app.jl
+++ src/app.jl
@@ -3,9 +3,7 @@ import ./layout
 import DashBootstrapComponents as dbc
 
 app = Dash.dash()
-
 app.config.external_stylesheets = ["dbc.themes.BOOTSTRAP"]
-
 app.layout = layout.create_layout()
 
 Dash.run_server(app, "0.0.0.0", debug=true)
--- src/layout.jl
+++ src/layout.jl
@@ -1,6 +1,12 @@
 module layout
 
 import Dash
+import ./components as Cpn
 
 export create_layout = Dash.html_div() do
+    Dash.title("My Gochess Page")
+    Cpn.render_canvas()
+    Cpn.render_span()
+    Cpn.render_restart()
+    Cpn.render_endcheck()
 end
