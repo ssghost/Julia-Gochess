ccca09f5995986f8809add2148dbd7e4afc6d207
ssghost <ssprof0@gmail.com>
2022-08-23 23:41:31 +0800

    .

--- src/app.jl
+++ src/app.jl
@@ -1,9 +1,9 @@
 import Dash
 import ./layout
-import DashBootstrapComponents as dbc
+import DashBootstrapComponents as Dbc
 
 app = Dash.dash()
-app.config.external_stylesheets = ["dbc.themes.BOOTSTRAP"]
+app.config.external_stylesheets = ["Dbc.themes.BOOTSTRAP"]
 app.layout = layout.create_layout()
 
 Dash.run_server(app, "0.0.0.0", debug=true)
